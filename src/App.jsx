8622530525383727ab4efc73cebf40e17927c663
Flávia Reis <flaviarster@gmail.com>
2024-05-02 16:24:32 -0300

    Ainda tentando atualizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { CatLists } from './components/CatLists';
 import { Header } from './components/Header';
+import { CatLists } from './components/CatLists';
 
 export function App() {
   return (
     <Router>
       <div className="w-full flex flex-col justify-center items-center m-auto">
         <Header />
+        {/* Defina a rota para /cats aqui */}
         <Route path="/cats" component={CatLists} />
       </div>
     </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { GlobalProvider } from './GlobalContext.jsx';
 import { App } from './App.jsx';
-import { Route } from 'react-router-dom';
 import { CatLists } from './components/CatLists.jsx';
 
 const AppWithRoutes = (
   <GlobalProvider>
-    <Route exact path="/" component={App} />
-    <Route path="/login" component={() => <div>Testando rotas</div>} />
-    <Route path="/cats" component={CatLists} />
+    <Router>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={() => <div>Testando rotas</div>} />
+      <Route path="/cats" component={CatLists} />
+    </Router>
   </GlobalProvider>
 );
 
