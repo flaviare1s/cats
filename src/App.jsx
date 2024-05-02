c738ee3e957db4d7dffd93a3d3929e07f8ceae10
Flávia Reis <flaviarster@gmail.com>
2024-05-02 16:55:24 -0300

    Ainda tentando configurar as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Header } from './components/Header';
+import { BrowserRouter as Route } from 'react-router-dom';
 import { CatLists } from './components/CatLists';
 
 export function App() {
   return (
-    <Router>
-      <div className="w-full flex flex-col justify-center items-center m-auto">
-        <Header />
-        {/* Defina a rota para /cats aqui */}
-        <Route path="/cats" component={CatLists} />
-      </div>
-    </Router>
+    <div className="w-full flex flex-col justify-center items-center m-auto">
+      <Header />
+      <Route path="/cats" component={CatLists} />
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { GlobalProvider } from './GlobalContext.jsx';
 import { App } from './App.jsx';
-import { CatLists } from './components/CatLists.jsx';
 
 const AppWithRoutes = (
   <GlobalProvider>
     <Router>
       <Route exact path="/" component={App} />
       <Route path="/login" component={() => <div>Testando rotas</div>} />
-      <Route path="/cats" component={CatLists} />
     </Router>
   </GlobalProvider>
 );
