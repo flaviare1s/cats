ed3b48f82af0db2bcafce0026caee0d61b30861b
Flávia Reis <flaviarster@gmail.com>
2024-05-02 15:13:51 -0300

    Corrigi os estilos do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ export const Header = () => {
 //   };
 
   return (
-    <div className='bg-green-500 text-white p-5 flex flex-row justify-between items-center w-full fixed mt-13'>
+    <div className='bg-green-500 text-white p-5 flex flex-row justify-between items-center w-full fixed mt-16'>
       <a href="#">Cats List</a>
       <div className='flex'>
         <button className='mr-3 border border-white rounded-lg px-4'>+ Add a cat</button>
