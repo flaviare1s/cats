0fff68797ccdc13147e9f46208186286cd0e72ae
Eduardo Caires Silva <eduardo0883@DIGITALCOLLEGE.LOCAL>
2024-05-02 15:40:53 -0300

    Envio para main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
     <div className='bg-green-500 text-white p-5 flex flex-row justify-between items-center w-full fixed mt-16'>
       <a href="#">Cats List</a>
       <div className='flex'>
-        <button className='mr-3 border border-white rounded-lg px-4'>+ Add a cat</button>
+        <button className='mr-3 border border-white rounded-lg px-4'>Add a cat</button>
         <div className='w-10 h-10 rounded-full border border-white flex items-center justify-center'><i className="fa-solid fa-user"></i></div>
       </div>
     </div>
